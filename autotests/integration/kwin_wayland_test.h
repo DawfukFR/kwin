16f81a8aab94a337c359ccac51a3d44b6d14f580
Marco Martin <notmart@gmail.com>
2024-03-28 10:31:14 +0100

    quick tile test less racy

--- autotests/integration/kwin_wayland_test.h
+++ autotests/integration/kwin_wayland_test.h
@@ -242,6 +242,7 @@ public:
         TiledBottom = 0x100,
         Suspended = 0x200,
         Maximized = MaximizedHorizontal | MaximizedVertical,
+        TiledMaximized = TiledLeft | TiledTop | TiledRight | TiledBottom
     };
     Q_DECLARE_FLAGS(States, State)
 
